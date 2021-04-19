87e1b541a62c5a0fe6ab977476f298705468aa04
fursik <19dima991999@mail.ru>
2021-04-19 18:10:07 +0300

    hit test example

--- js/createScene.js
+++ js/createScene.js
@@ -2,15 +2,12 @@ const canvas = document.getElementById("renderCanvas"); // Get the canvas elemen
 const engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
 
 var createScene = async function () {
+
   // This creates a basic Babylon Scene object (non-mesh)
   var scene = new BABYLON.Scene(engine);
 
   // This creates and positions a free camera (non-mesh)
-  var camera = new BABYLON.FreeCamera(
-    "camera1",
-    new BABYLON.Vector3(0, 1, -5),
-    scene
-  );
+  var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 1, -5), scene);
 
   // This targets the camera to scene origin
   camera.setTarget(BABYLON.Vector3.Zero());
@@ -19,68 +16,38 @@ var createScene = async function () {
   camera.attachControl(canvas, true);
 
   // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-  var light = new BABYLON.HemisphericLight(
-    "light",
-    new BABYLON.Vector3(0, 1, 0),
-    scene
-  );
+  var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
 
   // Default intensity is 1. Let's dim the light a small amount
   light.intensity = 0.7;
 
   var xr = await scene.createDefaultXRExperienceAsync({
-    uiOptions: {
-      sessionMode: "immersive-ar",
-    },
-    optionalFeatures: true,
+      uiOptions: {
+          sessionMode: "immersive-ar",
+      },
+      optionalFeatures: true
   });
 
   const fm = xr.baseExperience.featuresManager;
 
   const xrTest = fm.enableFeature(BABYLON.WebXRHitTest, "latest");
 
-  const markersArray = createObjects();
-  const markers = BABYLON.Mesh.MergeMeshes(markersArray);
+  const marker = BABYLON.MeshBuilder.CreateTorus('marker', { diameter: 0.15, thickness: 0.05 });
+  marker.isVisible = false;
+  marker.rotationQuaternion = new BABYLON.Quaternion();
 
   xrTest.onHitTestResultObservable.add((results) => {
-    if (results.length) {
-      markers.isVisible = true;
-      hitTest = results[0];
-      hitTest.transformationMatrix.decompose(
-        markers.scaling,
-        markers.rotationQuaternion,
-        markers.position
-      );
-    } else {
-      markers.isVisible = false;
-    }
-  });
-
-  scene.onPointerObservable.add((pointerInfo) => {
-    switch (pointerInfo.type) {
-      case BABYLON.PointerEventTypes.POINTERDOWN:
-        if (pointerInfo.pickInfo.hit) {
-          pointerDown(pointerInfo.pickInfo.pickedMesh);
-        }
-        break;
-      case BABYLON.PointerEventTypes.POINTERUP:
-        break;
-      case BABYLON.PointerEventTypes.POINTERMOVE:
-        break;
-    }
-  });
-
-  // Register a render loop to repeatedly render the scene
-  engine.runRenderLoop(function () {
-    scene.render();
-  });
-
-  // Watch for browser/canvas resize events
-  window.addEventListener("resize", function () {
-    engine.resize();
+      if (results.length) {
+          marker.isVisible = true;
+          hitTest = results[0];
+          hitTest.transformationMatrix.decompose(marker.scaling, marker.rotationQuaternion, marker.position);
+      } else {
+          marker.isVisible = false;
+      }
   });
 
   return scene;
+
 };
 
 const pointerDown = function (mesh) {
